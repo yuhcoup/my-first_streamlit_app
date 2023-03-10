a824d8945ab07ca313c6112bfb02fcb9b1ec4626
yuhcoup <125709791+yuhcoup@users.noreply.github.com>
2023-03-11 00:40:41 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Giải phương trình bậc nhất")
+a=st.number_input("Tham số a")
+b = st.number_input("Tham số b")
+if st.button("Giải"):
+  if a == 0:
+    if b == 0:
+      st.write("Phương trình có vô số nghiệm")
+    else:
+      st.write("Phương trình vô nghiệm")
+  else:
+    st.write("Phương trình có 1 nghiệm", -b/a) 
